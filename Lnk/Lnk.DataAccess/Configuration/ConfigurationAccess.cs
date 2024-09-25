58086e98e58527dba22dbe59312e9cc935f03e7e
Lê Nhựt Khánh <lnkhanhtdtu@gmail.com>
2024-09-26 00:30:31 +0700

    Init

---
+++ Lnk/Lnk.DataAccess/Configuration/ConfigurationAccess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lnk.DataAccess.DataAccess;
+using Lnk.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lnk.DataAccess.Configuration
+{
+    public static class ConfigurationAccess
+    {
+        public static void RegisterDb(this IServiceCollection service, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection")
+                                   ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            service.AddDbContext<LnkDbContext>(options => options.UseSqlServer(connectionString));
+
+            service.AddIdentity<ApplicationUser, IdentityRole>() //(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<LnkDbContext>();
+        }
+    }
+}
---
+++ Lnk/Lnk.DataAccess/DataAccess/LnkDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Lnk.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lnk.DataAccess.DataAccess
+{
+    public class LnkDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public LnkDbContext(DbContextOptions<LnkDbContext> options) : base(options) { }
+    }
+}
